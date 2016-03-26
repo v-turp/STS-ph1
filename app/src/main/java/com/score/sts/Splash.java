f6bdb8fe4fc2ce1e38fe1eddb2042f705f0c6c97
victory.turpin <vturp@yahoo.com>
2016-03-25 21:08:12 -0700

    Completed entry and exit animation for v16+. Modified the splash layouts by making the images scale to fit the screen.

--- app/src/main/java/com/score/sts/Splash.java
+++ app/src/main/java/com/score/sts/Splash.java
@@ -1,8 +1,11 @@
 package com.score.sts;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
 import android.view.View;
 
 import butterknife.ButterKnife;
@@ -18,27 +21,38 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.splash);
         ButterKnife.bind(this);
 
-        /**
-         * this enables fullscreen for version 4.1+
-         * set view hidden step 1
-         * */
+        init();
+    } // end method onCreate
+
+    private void init() {
+
+        //--- this this enables fullscreen for version 4.1+  set view hidden step 1
         View decorView = getWindow().getDecorView();
-        // Hide the status bar.
+        //--- hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         // --- end fullscreen impl 4.1+
 
-        //---set the delay for calling the next activity
+
+        //--- set the animation based on version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setExitTransition(new Explode());
+        }
+
+        //--- set the delay for calling the next activity, set transition for v21+
         new View(this).postDelayed(new Runnable() {
 
             @Override
             public void run() {
                 Intent intent = new Intent(getBaseContext(), ManageContacts.class);
-                startActivity(intent);
+                if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(Splash.this).toBundle());
+                }else
+                    startActivity(intent);
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.slide_out_right);
             }
-        }, 5000);
-    } // end method onCreate
-
+        }, 3000);
+    }
 } // end class Splash
 
 
