dcd6ec96017479319b5bb6b4902b1af6c7e93154
victory.turpin <vturp@yahoo.com>
2016-03-26 12:13:04 -0700

    Clean-up commit. This commit is for all files that were requesting to be added to git. These files most likely contain local property changes.

--- app/build/intermediates/classes/debug/com/score/sts/ContactFrag$$ViewBinder.java
+++ app/build/intermediates/classes/debug/com/score/sts/ContactFrag$$ViewBinder.java
@@ -8,8 +8,8 @@ import butterknife.ButterKnife.ViewBinder;
 public class ContactFrag$$ViewBinder<T extends com.score.sts.ContactFrag> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131296375, "field 'rvContact'");
-    target.rvContact = finder.castView(view, 2131296375, "field 'rvContact'");
+    view = finder.findRequiredView(source, 2131296377, "field 'rvContact'");
+    target.rvContact = finder.castView(view, 2131296377, "field 'rvContact'");
   }
 
   @Override public void unbind(T target) {
--- app/build/intermediates/classes/debug/com/score/sts/LandingPage$$ViewBinder.java
+++ app/build/intermediates/classes/debug/com/score/sts/LandingPage$$ViewBinder.java
@@ -8,15 +8,15 @@ import butterknife.ButterKnife.ViewBinder;
 public class LandingPage$$ViewBinder<T extends com.score.sts.LandingPage> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131296402, "field 'rvLandingDrawer'");
-    target.rvLandingDrawer = finder.castView(view, 2131296402, "field 'rvLandingDrawer'");
-    view = finder.findRequiredView(source, 2131296399, "field 'etUsername'");
-    target.etUsername = finder.castView(view, 2131296399, "field 'etUsername'");
-    view = finder.findRequiredView(source, 2131296400, "field 'etPassword'");
-    target.etPassword = finder.castView(view, 2131296400, "field 'etPassword'");
-    view = finder.findRequiredView(source, 2131296398, "field 'ivLandingLogo'");
-    target.ivLandingLogo = finder.castView(view, 2131296398, "field 'ivLandingLogo'");
-    view = finder.findRequiredView(source, 2131296401, "method 'loginInUser'");
+    view = finder.findRequiredView(source, 2131296404, "field 'rvLandingDrawer'");
+    target.rvLandingDrawer = finder.castView(view, 2131296404, "field 'rvLandingDrawer'");
+    view = finder.findRequiredView(source, 2131296401, "field 'etUsername'");
+    target.etUsername = finder.castView(view, 2131296401, "field 'etUsername'");
+    view = finder.findRequiredView(source, 2131296402, "field 'etPassword'");
+    target.etPassword = finder.castView(view, 2131296402, "field 'etPassword'");
+    view = finder.findRequiredView(source, 2131296400, "field 'ivLandingLogo'");
+    target.ivLandingLogo = finder.castView(view, 2131296400, "field 'ivLandingLogo'");
+    view = finder.findRequiredView(source, 2131296403, "method 'loginInUser'");
     view.setOnClickListener(
       new butterknife.internal.DebouncingOnClickListener() {
         @Override public void doClick(
