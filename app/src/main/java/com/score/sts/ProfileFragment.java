ee6f3a9992298cfdb44f30a2289a4f67e956b773
victory.turpin <vturp@yahoo.com>
2016-04-05 01:13:30 -0400

    Made changes to adjust the margins on the landing page. Before this change, the star in each fragment was either not being shown or the star was out of place. The initial margin values from the xml layout did not carry over.

--- app/src/main/java/com/score/sts/ProfileFragment.java
+++ app/src/main/java/com/score/sts/ProfileFragment.java
@@ -1,12 +1,16 @@
 package com.score.sts;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -21,6 +25,20 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View profileView = inflater.inflate(R.layout.profile_fragment, container, false);
         ButterKnife.bind(this, getActivity());
+        setPortraitOrientationMargins(profileView);
         return profileView;
+    }
+
+    /**
+     * The star is not aligned correctly on portrait mode. this method
+     * dynamically sets the margin for the star icon
+     */
+    private void setPortraitOrientationMargins(View view){
+        if(getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            ImageView imageView = (ImageView) view.findViewById(R.id.ivSignUpStar);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(35,10,0,0);
+            imageView.setLayoutParams(layoutParams);
+        }
     }
 }
--- app/src/main/java/com/score/sts/RegisterWorkFragment.java
+++ app/src/main/java/com/score/sts/RegisterWorkFragment.java
@@ -1,11 +1,14 @@
 package com.score.sts;
 
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 /**
@@ -20,10 +23,23 @@ public class RegisterWorkFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.register_work_fragment, container, false);
+        View view = inflater.inflate(R.layout.register_work_fragment, container, false);
+        setPortraitOrientationMargins(view);
+        return view;
     }
 
+    /**
+     * The star is not aligned correctly on portrait mode. this method
+     * dynamically sets the margin for the star icon
+     */
+    private void setPortraitOrientationMargins(View view){
+        if(getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            ImageView imageView = (ImageView) view.findViewById(R.id.ivRegisterWorkStar);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(125,10,0,0);
+            imageView.setLayoutParams(layoutParams);
+        }
+    }
 }
